d2c9b8c2ec3b598fb39cb970fdc7413caee1632b
To Thanh Dat <ttdat2419@clc.fitus.edu.vn>
2025-05-18 16:11:54 +0700

    Update data.ts

--- app/data.ts
+++ app/data.ts
@@ -29,14 +29,14 @@ type SocialLink = {
 
 export const EMAIL = ''
 
-export const WEB_TITLE = 'Name - Personal Website'
+export const WEB_TITLE = 'Dat - Personal Website'
 export const WEB_DESCRIPTION =
   'A personal website showcasing my work and projects.'
-export const FOOTER_COPYRIGHT = '© 2025 Nguyen Van B.'
-export const FOOTER_LINK = 'https://github.com/username/'
+export const FOOTER_COPYRIGHT = '© 2025 To Thanh Dat.'
+export const FOOTER_LINK = 'https://github.com/tothanhdat2006/'
 
-export const NAME = 'Nguyen Van B'
-export const JOB_TITLE = 'NASA Frontend Intern'
+export const NAME = 'To Thanh Dat'
+export const JOB_TITLE = 'Sinh vien'
 export const DESCRIPTION =
   'I am a passionate designer and developer with a focus on creating beautiful and functional user interfaces. I have experience in design engineering, front-end development, and product design. I love to explore the intersection of design and technology, and I am always looking for new challenges to tackle.'
 
